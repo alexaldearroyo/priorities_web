8a15d3d1a900fa4961b5b0a3c00b64db31ba3895
alexaldearroyo <alexaldearroyo@gmail.com>
2023-11-03 15:30:12 +0900

    Refactoring made

--- src/js/main.js
+++ src/js/main.js
@@ -7,11 +7,13 @@ import {
   loadTasksFromLocalStorage,
 } from "./localStorageManager.js";
 
-import { 
+import {
+  handleAddTaskClick,
+  handleTaskCompletion,
   createTaskElement,
   removeTask,
   displayAllTasks,
-  filterTasksByPriority
+  filterTasksByPriority,
 } from "./taskManager.js";
 
 import {
@@ -22,187 +24,50 @@ import {
 
 import { displayCalendarWithTasks } from "./calendarManager.js";
 
-import {
-  PRIORITIES,
-  TASK_INPUT_PLACEHOLDER,
-  PRIORITY_LABEL,
-  DATE_LABEL,
-  ADD_BUTTON_TEXT,
-  CANCEL_BUTTON_TEXT,
-  COMPLETE_BUTTON_TEXT,
-} from "./constants.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    setupProjectEnvironment();
-    setupTaskEnvironment();
-    setupPrioritiesSubMenu();
-    setupProjectsLabel();
-    setupDatesLabel();
-    setupSidebarItems();
-    setupSidebarToggle();
-    displayAllTasks();
-    const calendarContainer = document.getElementById("calendarContainer");
-
-
+  setupProjectEnvironment();
+  setupTaskEnvironment();
+  setupPrioritiesSubMenu();
+  setupProjectsLabel();
+  setupDatesLabel();
+  setupSidebarItems();
+  setupSidebarToggle();
+  displayAllTasks();
+  const calendarContainer = document.getElementById("calendarContainer");
 });
 
-
 function setupProjectEnvironment() {
-    const createProjectButton = document.getElementById("createProjectButton");
-    const projectsContainer = document.getElementById("projectsContainer");
-    createProjectButton.style.display = "none";
-    setupCreateProjectButton(createProjectButton, projectsContainer);
+  const createProjectButton = document.getElementById("createProjectButton");
+  const projectsContainer = document.getElementById("projectsContainer");
+  createProjectButton.style.display = "none";
+  setupCreateProjectButton(createProjectButton, projectsContainer);
 }
 
 function setupTaskEnvironment() {
-    const addTaskButton = document.getElementById("addTaskButton");
-    const tasksContainer = document.getElementById("tasks");
+  const addTaskButton = document.getElementById("addTaskButton");
+  const tasksContainer = document.getElementById("tasks");
 
-    // Hide the "Create Project" button initially
-    addTaskButton.style.display = "none";
+  // Hide the "Create Project" button initially
+  addTaskButton.style.display = "none";
 
-    // Add event listener to detect when a task is marked as complete
-    tasksContainer.addEventListener("click", handleTaskCompletion);
+  // Add event listener to detect when a task is marked as complete
+  tasksContainer.addEventListener("click", handleTaskCompletion);
 
-    // Load and display existing tasks from local storage
-    // loadAndDisplayTasks(tasksContainer);
-
-    // Add event listener for the "Add Task" button
-    addTaskButton.addEventListener("click", handleAddTaskClick);
+  // Add event listener for the "Add Task" button
+  addTaskButton.addEventListener("click", handleAddTaskClick);
 }
 
-function handleTaskCompletion(event) {
-    if (event.target.classList.contains("complete-button")) {
-        removeTask(event.target);
-    }
-}
+
 
 function loadAndDisplayTasks(tasksContainer) {
-    const storedTasks = loadTasksFromLocalStorage();
-    for (const taskData of storedTasks) {
-        tasksContainer.appendChild(createTaskElement(taskData));
-    }
+  const storedTasks = loadTasksFromLocalStorage();
+  for (const taskData of storedTasks) {
+    tasksContainer.appendChild(createTaskElement(taskData));
+  }
 }
 
-function handleAddTaskClick() {
-  const addTaskButton = document.getElementById("addTaskButton");
-  const createProjectButton = document.getElementById("createProjectButton"); // Moved this line up for reference
 
-  // Initially hide the "Create Project" button
-  createProjectButton.style.display = "none";
-
- // Reference to the main tasks container
- const tasksContainer = document.getElementById("tasks");
-  // tasksContainer.addEventListener("click", (event) => {
-  //   if (event.target.classList.contains("complete-button")) {
-  //     removeTask(event.target);
-  //   }
-  // });
-
-  // Load existing tasks from local storage and display them
-  // const storedTasks = loadTasksFromLocalStorage();
-  // for (const taskData of storedTasks) {
-  //   tasksContainer.appendChild(createTaskElement(taskData));
-  // }
-
-  // Event listener for the "Add Task" button
-  // addTaskButton.addEventListener("click", () => {
-    // Disable the "Add Task" button to prevent multiple task additions
-    addTaskButton.disabled = true;
-
-    // Create new task input form elements
-    const taskElement = document.createElement("div");
-    taskElement.classList.add("new-task");
-
-    // Text input
-    const textInput = document.createElement("input");
-    textInput.type = "text";
-    textInput.placeholder = TASK_INPUT_PLACEHOLDER;
-    textInput.classList.add("task-input");
-
-    const actionContainer = document.createElement("div");
-    actionContainer.classList.add("action-container");
-
-    // Priority select
-    const priorityContainer = document.createElement("div");
-    priorityContainer.classList.add("custom-container", "priority-container");
-    const priorityLabel = document.createElement("label");
-    priorityLabel.textContent = PRIORITY_LABEL;
-    const prioritySelect = document.createElement("select");
-    PRIORITIES.forEach((priority) => {
-      const option = document.createElement("option");
-      option.value = priority;
-      option.textContent = priority;
-      prioritySelect.appendChild(option);
-    });
-    priorityContainer.appendChild(priorityLabel);
-    priorityContainer.appendChild(prioritySelect);
-
-    // Date input
-    const dateContainer = document.createElement("div");
-    dateContainer.classList.add("custom-container", "date-container");
-    const dateLabel = document.createElement("label");
-    dateLabel.textContent = DATE_LABEL;
-    const dateInput = document.createElement("input");
-    dateInput.type = "date";
-    dateContainer.appendChild(dateLabel);
-    dateContainer.appendChild(dateInput);
-
-    // Buttons
-    const buttonsContainer = document.createElement("div");
-    buttonsContainer.classList.add("buttons-container");
-
-    const addButton = document.createElement("button");
-    addButton.textContent = ADD_BUTTON_TEXT;
-    buttonsContainer.appendChild(addButton);
-
-    const cancelButton = document.createElement("button");
-    cancelButton.textContent = CANCEL_BUTTON_TEXT;
-    buttonsContainer.appendChild(cancelButton);
-
-    // Add the input elements to the task element
-    const inputGroup = document.createElement("div");
-    inputGroup.classList.add("input-group");
-    inputGroup.appendChild(textInput);
-    taskElement.appendChild(inputGroup);
-
-    actionContainer.appendChild(priorityContainer);
-    actionContainer.appendChild(dateContainer);
-    actionContainer.appendChild(buttonsContainer);
-
-    taskElement.appendChild(actionContainer);
-
-    // Event listeners for adding and cancelling the creation of new tasks
-    addButton.addEventListener("click", () => {
-      if (textInput.value) {
-        const taskTimestamp = Date.now();
-        const taskData = {
-          timestamp: taskTimestamp,
-          text: textInput.value,
-          priority: prioritySelect.value,
-          date: dateInput.value,
-        };
-        tasksContainer.appendChild(createTaskElement(taskData));
-
-        // Add task data to local storage
-        const tasks = loadTasksFromLocalStorage();
-        tasks.push(taskData);
-        saveTasksToLocalStorage(tasks);
-
-        taskElement.remove();
-        addTaskButton.disabled = false;
-      }
-    });
-
-    // Handle task cancellation
-    cancelButton.addEventListener("click", () => {
-      taskElement.remove();
-      addTaskButton.disabled = false;
-    });
-
-    // Append new task element to tasks container
-    tasksContainer.prepend(taskElement);
-}
 
 function setupPrioritiesSubMenu() {
   const prioritiesToggle = document.getElementById("togglePriorities");
@@ -223,7 +88,7 @@ function setupPrioritiesSubMenu() {
       const selectedPriority = event.target.textContent;
       filterTasksByPriority(selectedPriority);
       const tasksContainer = document.getElementById("tasks");
-      tasksContainer.style.display = "block";      
+      tasksContainer.style.display = "block";
       const projectsContainer = document.getElementById("projectsContainer");
       const calendarContainer = document.getElementById("calendarContainer");
       projectsContainer.style.display = "none";
@@ -235,24 +100,21 @@ function setupPrioritiesSubMenu() {
 function setupProjectsLabel() {
   const projectsLabel = document.querySelector("#menuProjects");
   let projectsAreShown = false;
-  const projectsContainer = document.getElementById(
-    "projectsContainer"
-  );
+  const projectsContainer = document.getElementById("projectsContainer");
   projectsContainer.classList.add("project-container");
   projectsLabel.addEventListener("click", () => {
-
     if (!projectsAreShown) {
       const addTaskButton = document.getElementById("addTaskButton");
       if (addTaskButton) {
         addTaskButton.style.display = "none";
       }
-  
-      projectsContainer.style.display = "block";  
+
+      projectsContainer.style.display = "block";
       loadAndDisplaySavedProjects(projectsContainer);
-  
+
       const tasksContainer = document.getElementById("tasks");
       tasksContainer.style.display = "none";
-        projectsAreShown = true;
+      projectsAreShown = true;
     }
   });
 }
@@ -274,7 +136,7 @@ function setupSidebarItems() {
   sidebarItems.forEach((item) => {
     item.addEventListener("click", (event) => {
       const calendarContainer = document.getElementById("calendarContainer");
-  
+
       switch (event.target.textContent) {
         case "Projects":
           createProjectButton.style.display = "block";
@@ -284,7 +146,7 @@ function setupSidebarItems() {
         case "Dates":
           createProjectButton.style.display = "none";
           projectsContainer.style.display = "none";
-  
+
           const allTasks = loadTasksFromLocalStorage();
           const tasksContainer = document.getElementById("tasks");
           displayCalendarWithTasks(tasksContainer, calendarContainer, allTasks);
@@ -297,13 +159,12 @@ function setupSidebarItems() {
       }
     });
   });
-  
 }
 
 function setupSidebarToggle() {
-    const toggleSidebarButton = document.getElementById("toggleSidebar");
-    const sidebar = document.querySelector("aside");
-    toggleSidebarButton.addEventListener("click", () => {
-        sidebar.classList.toggle("hidden");
-    });
+  const toggleSidebarButton = document.getElementById("toggleSidebar");
+  const sidebar = document.querySelector("aside");
+  toggleSidebarButton.addEventListener("click", () => {
+    sidebar.classList.toggle("hidden");
+  });
 }
--- src/js/taskManager.js
+++ src/js/taskManager.js
@@ -1,41 +1,152 @@
-import { saveTasksToLocalStorage, loadTasksFromLocalStorage } from './localStorageManager.js';
+import {
+  saveTasksToLocalStorage,
+  loadTasksFromLocalStorage,
+} from "./localStorageManager.js";
+import {
+  TASK_INPUT_PLACEHOLDER,
+  PRIORITY_LABEL,
+  DATE_LABEL,
+  ADD_BUTTON_TEXT,
+  CANCEL_BUTTON_TEXT,
+} from "./constants.js";
+
+export function displayAllTasks() {
+  const addTaskButton = document.getElementById("addTaskButton");
+  addTaskButton.style.display = "flex"; // Shows "add task" button
+  const allTasks = loadTasksFromLocalStorage();
+  const tasksContainer = document.getElementById("tasks");
+  tasksContainer.innerHTML = ""; // Clear the current tasks
+  for (const taskData of allTasks) {
+    tasksContainer.appendChild(createTaskElement(taskData));
+  }
+  tasksContainer.style.display = "block";
+}
 
 /**
- * Display all tasks.
+ * Filter tasks by their priority.
+ * @param {string} priority - The selected priority.
  */
-export function displayAllTasks() {
-    const addTaskButton = document.getElementById("addTaskButton");
-    addTaskButton.style.display = "flex"; // Shows "add task" button
-    const allTasks = loadTasksFromLocalStorage();
-    const tasksContainer = document.getElementById("tasks");
-    tasksContainer.innerHTML = ""; // Clear the current tasks
-    for (const taskData of allTasks) {
-      tasksContainer.appendChild(createTaskElement(taskData));
-    }
-    tasksContainer.style.display = "block";
+export function filterTasksByPriority(priority) {
+  const allTasks = loadTasksFromLocalStorage();
+  const filteredTasks = allTasks.filter((task) => task.priority === priority);
+  const tasksContainer = document.getElementById("tasks");
+  tasksContainer.innerHTML = ""; // Clear the current tasks
+  for (const taskData of filteredTasks) {
+    tasksContainer.appendChild(createTaskElement(taskData));
   }
-  
-  /**
-   * Filter tasks by their priority.
-   * @param {string} priority - The selected priority.
-   */
-  export function filterTasksByPriority(priority) {
-    const allTasks = loadTasksFromLocalStorage();
-    const filteredTasks = allTasks.filter((task) => task.priority === priority);
-    const tasksContainer = document.getElementById("tasks");
-    tasksContainer.innerHTML = ""; // Clear the current tasks
-    for (const taskData of filteredTasks) {
+  const addTaskButton = document.getElementById("addTaskButton");
+  addTaskButton.style.display = "none"; // Hide the "Add task" button
+}
+
+export function handleAddTaskClick() {
+  const addTaskButton = document.getElementById("addTaskButton");
+  const createProjectButton = document.getElementById("createProjectButton");
+
+  createProjectButton.style.display = "none";
+
+  const tasksContainer = document.getElementById("tasks");
+
+  addTaskButton.disabled = true;
+
+  // Create new task input form elements
+  const taskElement = document.createElement("div");
+  taskElement.classList.add("new-task");
+
+  // Text input
+  const textInput = document.createElement("input");
+  textInput.type = "text";
+  textInput.placeholder = TASK_INPUT_PLACEHOLDER;
+  textInput.classList.add("task-input");
+
+  const actionContainer = document.createElement("div");
+  actionContainer.classList.add("action-container");
+
+  // Priority select
+  const priorityContainer = document.createElement("div");
+  priorityContainer.classList.add("custom-container", "priority-container");
+  const priorityLabel = document.createElement("label");
+  priorityLabel.textContent = PRIORITY_LABEL;
+  const prioritySelect = document.createElement("select");
+  PRIORITIES.forEach((priority) => {
+    const option = document.createElement("option");
+    option.value = priority;
+    option.textContent = priority;
+    prioritySelect.appendChild(option);
+  });
+  priorityContainer.appendChild(priorityLabel);
+  priorityContainer.appendChild(prioritySelect);
+
+  // Date input
+  const dateContainer = document.createElement("div");
+  dateContainer.classList.add("custom-container", "date-container");
+  const dateLabel = document.createElement("label");
+  dateLabel.textContent = DATE_LABEL;
+  const dateInput = document.createElement("input");
+  dateInput.type = "date";
+  dateContainer.appendChild(dateLabel);
+  dateContainer.appendChild(dateInput);
+
+  // Buttons
+  const buttonsContainer = document.createElement("div");
+  buttonsContainer.classList.add("buttons-container");
+
+  const addButton = document.createElement("button");
+  addButton.textContent = ADD_BUTTON_TEXT;
+  buttonsContainer.appendChild(addButton);
+
+  const cancelButton = document.createElement("button");
+  cancelButton.textContent = CANCEL_BUTTON_TEXT;
+  buttonsContainer.appendChild(cancelButton);
+
+  // Add the input elements to the task element
+  const inputGroup = document.createElement("div");
+  inputGroup.classList.add("input-group");
+  inputGroup.appendChild(textInput);
+  taskElement.appendChild(inputGroup);
+
+  actionContainer.appendChild(priorityContainer);
+  actionContainer.appendChild(dateContainer);
+  actionContainer.appendChild(buttonsContainer);
+
+  taskElement.appendChild(actionContainer);
+
+  // Event listeners for adding and cancelling the creation of new tasks
+  addButton.addEventListener("click", () => {
+    if (textInput.value) {
+      const taskTimestamp = Date.now();
+      const taskData = {
+        timestamp: taskTimestamp,
+        text: textInput.value,
+        priority: prioritySelect.value,
+        date: dateInput.value,
+      };
       tasksContainer.appendChild(createTaskElement(taskData));
+
+      // Add task data to local storage
+      const tasks = loadTasksFromLocalStorage();
+      tasks.push(taskData);
+      saveTasksToLocalStorage(tasks);
+
+      taskElement.remove();
+      addTaskButton.disabled = false;
     }
-    const addTaskButton = document.getElementById("addTaskButton");
-    addTaskButton.style.display = "none"; // Hide the "Add task" button
+  });
+
+  // Handle task cancellation
+  cancelButton.addEventListener("click", () => {
+    taskElement.remove();
+    addTaskButton.disabled = false;
+  });
+
+  // Append new task element to tasks container
+  tasksContainer.prepend(taskElement);
+}
+
+export function handleTaskCompletion(event) {
+  if (event.target.classList.contains("complete-button")) {
+    removeTask(event.target);
   }
-  
-
-
-
-///// OLD
-
+}
 
 /**
  * Create a DOM element for a task.
@@ -43,46 +154,47 @@ export function displayAllTasks() {
  * @returns {HTMLElement} The task element.
  */
 export function createTaskElement(taskData) {
-    const task = document.createElement('div');
-    task.classList.add('task');
-    task.dataset.timestamp = taskData.timestamp;
+  const task = document.createElement("div");
+  task.classList.add("task");
+  task.dataset.timestamp = taskData.timestamp;
 
-    const taskText = document.createElement('div');
-    taskText.classList.add('task-text');
-    taskText.textContent = taskData.text;
-    task.appendChild(taskText);
+  const taskText = document.createElement("div");
+  taskText.classList.add("task-text");
+  taskText.textContent = taskData.text;
+  task.appendChild(taskText);
 
-    const taskDetails = document.createElement('div');
-    taskDetails.classList.add('task-details');
+  const taskDetails = document.createElement("div");
+  taskDetails.classList.add("task-details");
 
-    const priorityContainer = document.createElement('div');
-    priorityContainer.classList.add('custom-container', 'priority-container');
-    const prioritySpan = document.createElement('span');
-    prioritySpan.textContent = taskData.priority;
-    priorityContainer.appendChild(prioritySpan);
+  const priorityContainer = document.createElement("div");
+  priorityContainer.classList.add("custom-container", "priority-container");
+  const prioritySpan = document.createElement("span");
+  prioritySpan.textContent = taskData.priority;
+  priorityContainer.appendChild(prioritySpan);
 
-    if(taskData.date && taskData.date.trim() !== "") { // Comprobar si hay fecha y no está vacía
-        const dateContainer = document.createElement('div');
-        dateContainer.classList.add('custom-container', 'date-container');
-        const dateSpan = document.createElement('span');
-        dateSpan.textContent = taskData.date;
-        dateContainer.appendChild(dateSpan);
-        taskDetails.appendChild(dateContainer);
-    }
+  if (taskData.date && taskData.date.trim() !== "") {
+    // Comprobar si hay fecha y no está vacía
+    const dateContainer = document.createElement("div");
+    dateContainer.classList.add("custom-container", "date-container");
+    const dateSpan = document.createElement("span");
+    dateSpan.textContent = taskData.date;
+    dateContainer.appendChild(dateSpan);
+    taskDetails.appendChild(dateContainer);
+  }
 
-    const completeButton = document.createElement('button');
-    completeButton.classList.add('complete-button');
-    completeButton.textContent = "Complete";
-    completeButton.addEventListener("click", function() {
-      removeTask(this);
-    });
+  const completeButton = document.createElement("button");
+  completeButton.classList.add("complete-button");
+  completeButton.textContent = "Complete";
+  completeButton.addEventListener("click", function () {
+    removeTask(this);
+  });
 
-    taskDetails.appendChild(priorityContainer);
-    taskDetails.appendChild(completeButton);
+  taskDetails.appendChild(priorityContainer);
+  taskDetails.appendChild(completeButton);
 
-    task.appendChild(taskDetails);
+  task.appendChild(taskDetails);
 
-    return task;
+  return task;
 }
 
 /**
@@ -90,11 +202,13 @@ export function createTaskElement(taskData) {
  * @param {HTMLElement} buttonElement - The button element that triggered the removal.
  */
 export function removeTask(buttonElement) {
-    const taskElement = buttonElement.parentElement.parentElement;
-    const taskTimestamp = taskElement.dataset.timestamp;
+  const taskElement = buttonElement.parentElement.parentElement;
+  const taskTimestamp = taskElement.dataset.timestamp;
 
-    taskElement.remove();
+  taskElement.remove();
 
-    const tasks = loadTasksFromLocalStorage().filter(task => task.timestamp && task.timestamp.toString() !== taskTimestamp);
-    saveTasksToLocalStorage(tasks);
+  const tasks = loadTasksFromLocalStorage().filter(
+    (task) => task.timestamp && task.timestamp.toString() !== taskTimestamp
+  );
+  saveTasksToLocalStorage(tasks);
 }
